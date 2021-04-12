23b4319ed9cdb9f4d1493e51612db68466fe2ae1
ppassion <chenyanhui1087@163.com>
2021-04-12 22:25:56 +0800

    shell

---
+++ bigdata/all-stop.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+###
+ # @Author       : chyh
+ # @Date         : 2021-04-09 13:27:00
+ # @LastEditTime : 2021-04-09 23:38:58
+ # @Description  : Do not edit
+### 
+
+./hadoop.sh stop
+./zookeeper.sh stop
