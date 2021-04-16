5bfe83101e136c811418400e469472c59e2ef31f
ppassion <chenyanhui1087@163.com>
2021-04-16 22:51:57 +0800

    bigdata

--- bigdata/all-start.sh
+++ bigdata/all-start.sh
@@ -2,8 +2,8 @@
 ###
  # @Author       : chyh
  # @Date         : 2021-04-09 13:27:00
- # @LastEditTime : 2021-04-09 13:27:17
- # @Description  : Do not edit
+ # @LastEditTime : 2021-04-16 22:51:36
+ # @Description  : 启动所有组件，可用于重启集群后使用
 ### 
 
 ./hadoop.sh start
--- bigdata/all-stop.sh
+++ bigdata/all-stop.sh
@@ -2,8 +2,8 @@
 ###
  # @Author       : chyh
  # @Date         : 2021-04-09 13:27:00
- # @LastEditTime : 2021-04-09 23:38:58
- # @Description  : Do not edit
+ # @LastEditTime : 2021-04-16 22:51:49
+ # @Description  : 关闭所有组件，可用于关闭集群之前
 ### 
 
 ./hadoop.sh stop
